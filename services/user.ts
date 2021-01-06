b6925cf92d4ab890d6cb2a33258d75fe767945e4
Daniel Fernandes Vieira <danielvieiraufc@gmail.com>
2021-01-06 10:59:07 -0300

    Configurando nossa API

---
+++ services/user.ts
@@ -0,0 +1,36 @@
+import api from './api'
+import User from '../dtos/User'
+
+// criando os tipos - padrao do TypeScript
+interface SignInData {
+  email: string,
+  password: string
+}
+interface SignInResponse {
+  data: User
+}
+interface SignUpData {
+  name: string,
+  email: string,
+  password: string,
+  password_confirmation: string
+}
+
+const UsersService = {
+  signIn: ({ email, password }: SignInData) => {
+    api.post<SignInResponse>('auth/v1/user/sign_in', {
+      email,
+      password
+    })
+  },
+  signUp: ({ name, email, password, password_confirmation }: SignUpData) => {
+    api.post<void>('auth/v1/user/sign_up', {
+      name,
+      email,
+      password,
+      password_confirmation
+    })
+  }
+}
+
+export default UsersService
