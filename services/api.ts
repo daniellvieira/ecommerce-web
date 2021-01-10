37695dee27935f175776cfde452a9d81e6558f0e
Daniel Fernandes Vieira <danielvieiraufc@gmail.com>
2021-01-10 15:24:14 -0300

    Preparando o terreno para os CRUDS

--- services/api.ts
+++ services/api.ts
@@ -1,57 +1,78 @@
-import axios from 'axios'
-import Cookie from 'js-cookie'
-import ApiData from '../dtos/ApiData'
+import axios, { AxiosResponse } from 'axios';
+import Cookie from 'js-cookie';
+import ApiData from '../dtos/ApiData';
+import ApiResponseError from '../dtos/ApiReponseError';
+
+// importação do router para que possamos realizar o redirect caso o usuário
+// recebe a mensagem da api de que não acesso ao recurso que tentou acessar.
+import Router from 'next/router';
+import { toast } from 'react-toastify';
 
 const api = axios.create({
   baseURL: 'http://localhost:3000'
-})
+});
 
-// Interceptor de resposta
-// Toda vez que o axios realizar uma request e tiver uma resposta, esse código
-// será executado antes de devolver o response ao fluxo normal do código.
-// Para cada resposta, checamos se existem os headers que são necessários para
-// realizar a autenticação na api.
-// Se os mesmos existirem, criamos um objeto chamado apiData, setamos ele como
-// header padrão no objeto api e ainda, salvamos ele nos cookies do browser
-// *res => é a response que será devolvida pela requisição do axios 
-api.interceptors.response.use(res => {
-  if(res.headers['access-token']) {
+// adição da função para setar os headers de authẽnticação na api
+// e nos cookies do browser, iremos utilizar ela no interceptor de 
+// request (tanto no fluxo normal quando no fluxo de erro).
+function setHeaders(res: AxiosResponse<any>) {
+  if(res.headers['access-token'] && res.headers['access-token'] !== '') {
     const apiData: ApiData = {
       'access-token': res.headers['access-token'],
       client: res.headers.client,
       expiry: res.headers.expiry,
       'token-type': res.headers['token-type'],
       uid: res.headers.uid
-    }
+    };
 
-    api.defaults.headers = apiData
-    Cookie.set('@api-data', apiData)
+    api.defaults.headers = apiData;
+    Cookie.set('@api-data', apiData);
+  }
+}
+
+api.interceptors.response.use(res => {
+  setHeaders(res);
+  return res;
+}
+, err => {
+  // caso um erro ocorra na response, um novo token é retornado, logo devemos atualizá-lo na api e nos cookies
+  if (err.response) {
+    setHeaders(err.response);
+
+    const data = err.response.data;
+
+    // aqui estamos tratando os erros no padrão que o rails no devolve, se existem algum array de erros, iremos extrair o nome do campo e as mensagens para que as mesmas possam ser exibidas na tela utilizando um toast
+    if (data && data.errors && data.errors.fields) {
+      const errors = data.errors as ApiResponseError;
+
+      const fieldsName = Object.keys(errors.fields)
+
+      fieldsName.forEach(error => {
+        toast.error(error + ': ' + errors.fields[error].join(`, `))
+      })
+
+      console.log('errors', errors);
+    }
   }
 
-  return res
-})
+  // caso a response tenha um status de não autorizado ou acesso negado, o usuário será redirecionado para o login.
+  if (err.response && (
+      err.response.status === 401 ||
+      err.response.status === 403
+    )) {
+    Router.push('/Auth/Login');
+  }
 
-// Intercetpor de request
-// Toda vez que o axios realizar um request, antes do mesmo enviar a requisição,
-// esse código será executado.
-// Para cada request, checamos se a url contém o valor 'admin' (rota protegida do admin).
-// Caso seja uma rota do admin, recuperamos os dados de autenticação dos cookies
-// do browser e adicionamos na request.
-// Dessa forma simplificamos a forma de tratar os headers de autenticação, não sendo
-// preciso adicioná-los em cada request que necessite dos mesmos.
-// *req => é o request que será executado após esse código
+  throw err;
+});
+
 api.interceptors.request.use(req => {
   if(req.url.includes('admin')) {
     const apiData: ApiData = JSON.parse(Cookie.get('@api-data'));
-    req.headers = apiData
+    req.headers = apiData;
   }
 
-  return req
+  return req;
 })
 
-
-// Os interceptors do axios funcionando como middlewares que executam antes de devolver
-// a response do request ou antes do request ser realizado, dessa forma podemos alterar
-// os dados do response ou adicionar dados as request antes das mesmas serem executadas.
-// link de referência(en): https://masteringjs.io/tutorials/axios/interceptors
-export default api
+export default api;
